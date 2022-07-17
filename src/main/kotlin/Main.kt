d2d18411506fd59989a32e82b5b67fe59305e969
BillyLuisNeedham <billyluisneedham@gmail.com>
2022-07-17 20:35:05 +0100

    basic setup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+import main.kotlin.functions.calculatePrice
+import main.kotlin.models.Item
+import main.kotlin.models.ItemName
+import main.kotlin.models.ItemQuantity
+import main.kotlin.models.Offer
+import main.kotlin.models.PriceInPounds
+import main.kotlin.models.Promotion
+
 // TODO Needs to:
 //- Take a list of items {name: ItemName, price: PriceInPounds}
 //- Needs to take a list of promotions
@@ -7,7 +15,24 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    val price = calculatePrice(
-        items
+    val items = listOf(
+        Item(
+            name = ItemName("apple"),
+            price = PriceInPounds(0.65)
+        )
+    )
+    val promotions = listOf(
+        Promotion(
+            item = ItemName("apple"),
+            offer = Offer(
+                originalAmount = ItemQuantity(1),
+                offerAmount = ItemQuantity(2)
+            )
+        )
+    )
+
+    val price: PriceInPounds = calculatePrice(
+        items = items,
+        promotions = promotions
     )
 }
---
+++ src/main/kotlin/functions/calculatePrice.kt
@@ -0,0 +1,13 @@
+package main.kotlin.functions
+
+import main.kotlin.models.Item
+import main.kotlin.models.PriceInPounds
+import main.kotlin.models.Promotion
+
+// TODO test
+fun calculatePrice(
+    items: List<Item>,
+    promotions: List<Promotion>
+): PriceInPounds {
+    TODO("Not yet implemented")
+}
